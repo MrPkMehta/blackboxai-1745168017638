89d369e8123aaffcc3e939ea5446c8a88bea0d81
PRIYAMANSHU KUMAR <123830581+MrPkMehta@users.noreply.github.com>
2025-04-20 22:24:23 +0530

    Add src/main/java/com/example/ecommerceplatform/model/OrderItem.java 1745168063202

---
+++ src/main/java/com/example/ecommerceplatform/model/OrderItem.java
@@ -0,0 +1,55 @@
+package com.example.ecommerceplatform.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+
+    private double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Product product, int quantity, double price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
